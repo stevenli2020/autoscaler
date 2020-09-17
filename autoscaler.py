47b232a03cd1b0cf318d68ef3a0078cf3cae2d61
stevenli2020 <stevenli2020@github.com>
2020-09-17 06:16:21 +0000

    add scaling queue to handle multiple services

--- autoscaler.py
+++ autoscaler.py
@@ -2,6 +2,21 @@
 import os, string, random, time, socket, thread, sys, commands, json
 
 LEADER_ONLINE = True
+SCALING = False
+scaling_queue = []
+
+def scaler():
+	global scaling_queue
+	while 1:
+		try:
+			# print scaling_queue
+			if len(scaling_queue) > 0:
+				(SERVICE,REPLICAS) = scaling_queue.pop(0)
+				EXEC("docker service scale "+SERVICE+"="+str(REPLICAS))+"\n"
+				print "Service '"+SERVICE+"' scaled to "+str(REPLICAS)
+		except:
+			pass
+		time.sleep(2)	
 
 def UDP_ECHO():
 	while 1:
@@ -34,53 +49,77 @@ def CHECK_LEADER():
 			LEADER_ONLINE = False
 			print "- leader node is offline"
 		time.sleep(30)
-	pass
+
+def SERVICE_SCALER(SERVICE):
+	global LEADER_ONLINE, CONF
+	EC = [0]*int(SERVICE['MA_POINTS'])
+	while 1:
+		time.sleep(SERVICE['SAMPLE_INTERVAL'])
+		SERVICE_REPLICAS = int(EXEC("docker service ps "+SVC['SERVICE_NAME']+" -f desired-state=Running -q | wc -l"))
+		try:
+			ESTABLISHED_CONNECTIONS = int(EXEC("nsenter -t $(docker inspect -f '{{.State.Pid}}' $(docker ps --format {{.Names}} | grep "+SERVICE['SERVICE_NAME']+" | head -1)) -n netstat -pant | grep ESTA | wc -l"))
+		except:
+			ESTABLISHED_CONNECTIONS = 0
+		EC.append(ESTABLISHED_CONNECTIONS)
+		EC.pop(0) 
+		EC_AVG = sum(EC) / SERVICE['MA_POINTS']
+		# print "SERVICE_REPLICAS="+str(SERVICE_REPLICAS)+"; ESTABLISHED_CONNECTIONS="+','.join(map(str,EC))+"; AVG="+str(EC_AVG)  
+		print SERVICE['SERVICE_NAME']+"="+str(SERVICE_REPLICAS)+"; CONN="+str(EC_AVG)  
+		if EC_AVG > SERVICE['CONN_THRESHOLD_H']:
+			print "Scaling up triggered by upper threshold"
+			thread.start_new_thread(SERVICE_SCALE_UP,(SERVICE,SERVICE_REPLICAS))
+		elif (EC_AVG < SERVICE['CONN_THRESHOLD_L']) and (SERVICE_REPLICAS > SERVICE['BASE_REP_COUNT']):
+			print "Scaling down triggered by lower threshold"
+			thread.start_new_thread(SERVICE_SCALE_DN,(SERVICE,SERVICE_REPLICAS))		
+			
+def SERVICE_SCALE_UP(SVC, REP):
+	global SCALING, scaling_queue
+	if SCALING:
+		print "Within scaling stabilization time, skip"
+		return
+	SCALING = True
+	if REP+2 <= SVC['MAX_REP_COUNT']:
+		scaling_queue.append((SVC['SERVICE_NAME'],REP+2))
+	else:
+		print "Scaling target out of range"
+	time.sleep(SVC['STABLIZATION_TIME'])
+	SCALING = False
+	print "Scaling up completed"
+
+def SERVICE_SCALE_DN(SVC, REP):
+	global SCALING, scaling_queue
+	if SCALING:
+		print "Within scaling stabilization time, skip"
+		return
+	SCALING = True
+	if REP-2 >= SVC['BASE_REP_COUNT']:
+		scaling_queue.append((SVC['SERVICE_NAME'],REP-2))
+	else:
+		print "Scaling target out of range"	
+	time.sleep(SVC['STABLIZATION_TIME'])
+	SCALING = False	
+	print "Scaling down completed"
+	
+	
 #=====================================================
 
 with open("/etc/autoscaler/config", 'r') as f:
 	CONF = json.loads(f.read())
-print CONF
-exit()
-# thread.start_new_thread(UDP_ECHO,())
-thread.start_new_thread(API_SERVER,())
 
-if CONF['MODE'] != "LEADER":
+thread.start_new_thread(UDP_ECHO,())
+thread.start_new_thread(scaler,())
+
+if CONF['NODE_MODE'] != "LEADER":
 	thread.start_new_thread(CHECK_LEADER,())
+
+for SVC in CONF["AUTOSCALE"]:
+	print SVC['SERVICE_NAME']
+	thread.start_new_thread(SERVICE_SCALER,(SVC,))
 	
-REPLICATION = int(EXEC("docker service ps "+CONF['SERVICE']+" --format '{{.Name}}' | wc -l"))
-# REPLICATION = REPLICATION + 2
-# EXEC("docker service scale "+CONF['SERVICE']+"="+str(REPLICATION))
-# exit()
-PORTS = ""
-for P in CONF['PORTS']:
-	PORTS = PORTS+":"+str(P)+" |"
-PORTS = PORTS[:-1]
-# print PORTS
-T0 = time.time()-int(CONF['STABLIZATION'])
+	
 while 1:
-	time.sleep(CONF['INTERVAL'])
-	if CONF['MODE'] != "LEADER": 
-		if LEADER_ONLINE == True:
-			print "Leader node is online, skip"
-			continue
-	if CONF['AUTOSCALE'] != True:
-		continue
-	T1 = time.time()
-	CONNECTIONS = int(EXEC("docker exec $(docker ps -f NAME=$(docker service ps -f NODE="+CONF['NODE']+" "+CONF['SERVICE']+" --format '{{.Name}}' | head -1) --format '{{.Names}}') netstat -tan | grep -E '"+PORTS+"' | wc -l"))
-	print "Current number of containers: "+str(REPLICATION)+", connections on each: "+str(CONNECTIONS)
-	if T1 - T0 > int(CONF['STABLIZATION']):
-		if CONNECTIONS > CONF['CONN_THRESHOLD_H']:
-			REPLICATION = REPLICATION + 2
-			print "Scale up service '"+CONF['SERVICE']+"' by 2, REPLICATION="+str(REPLICATION)
-			EXEC("docker service scale "+CONF['SERVICE']+"="+str(REPLICATION))
-			print "Wait 30s for connection to be stablized..."
-			T0 = time.time()			
-		elif CONNECTIONS < CONF['CONN_THRESHOLD_L'] and REPLICATION != CONF['BASE_REPLICATION']:
-			REPLICATION = REPLICATION - 2
-			print "Scale down service '"+CONF['SERVICE']+"' by 2, REPLICATION="+str(REPLICATION)
-			EXEC("docker service scale "+CONF['SERVICE']+"="+str(REPLICATION))
-			print "Wait 30s for connection to be stablized..."
-			T0 = time.time()
+	time.sleep(2)
+	
 	
 	
 	
